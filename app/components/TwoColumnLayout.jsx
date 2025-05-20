f804f766cf2fc88f028bb442e87b32e61adc41d9
heliobei <suporte@beieducacaopedagogico.com>
2025-05-20 10:20:39 -0300

    Update TwoColumnLayout.jsx

--- app/components/TwoColumnLayout.jsx
+++ app/components/TwoColumnLayout.jsx
@@ -4,7 +4,7 @@ import ResourceCards from './ResourcesCards';
 
 const TwoColumnLayout = () => {
   return (
-    <div className="relative h-[80vh] flex items-center justify-center bg-gradient-to-br from-blue-900 to-green-300 z-0">
+    <div className="relative h-[95vh] flex items-center justify-center bg-gradient-to-br from-blue-900 to-green-300 z-0">
       
       <a
         href="/"
@@ -20,14 +20,23 @@ const TwoColumnLayout = () => {
         />
       </a>
 
+      <a
+        href="/"
+       
+        className="absolute top-4 right-4 z-10"
+      >
+        <Image
+          src="/seed_h.png"
+          alt="Logo SEED"
+          width={150}
+          height={70}
+          className="mx-8"
+        />
+      </a>
+
       <ResourceCards className="z-100"/>
     </div>
   );
 };
 
 export default TwoColumnLayout;
-
-
-
-
-
